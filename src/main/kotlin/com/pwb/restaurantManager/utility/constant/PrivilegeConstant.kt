4d0b183c227f26ce92b97398c5999b730ca92599
QuCoon AI, DA & ML <machinelearningteam@qucoon.com>
2025-03-21 19:09:34 +0100

    Initial commit for RestaurantManager

---
+++ src/main/kotlin/com/pwb/restaurantManager/utility/constant/PrivilegeConstant.kt
@@ -0,0 +1,11 @@
+//TODO ==== BUILT WITH QOONITY VERSION-1.0 - YINKA-ANTHONY-NKAY ON 2025-03-21T18:08:05.851191600
+package com.pwb.restaurantManager.utility.constant
+
+object PrivilegeConstant {
+    const val CREATE = "CREATE"
+    const val UPDATE = "UPDATE"
+    const val READ = "READ"
+    const val DELETE = "DELETE"
+    const val CHECKER = "CHECKER"
+    const val ALL = "*"
+}
