c8ab11f1ba9a2957883cbc1f3c0b7ebf2110a2aa
QuCoon AI, DA & ML <machinelearningteam@qucoon.com>
2025-03-21 19:09:23 +0100

    Initial commit for RestaurantManager

---
+++ src/main/kotlin/com/pwb/restaurantManager/RestaurantManagerApplication.kt
@@ -0,0 +1,13 @@
+//TODO ==== BUILT WITH QOONITY VERSION-1.0 - YINKA-ANTHONY-NKAY ON 2025-03-21T18:08:05.851191600
+package com.pwb.restaurantManager
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RestaurantManagerApplication
+
+fun main(args: Array<String>) {
+	runApplication< RestaurantManagerApplication>(*args)
+}
+
