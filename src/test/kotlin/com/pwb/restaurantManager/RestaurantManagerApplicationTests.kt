907cb02b6c6fc0274ea4f1163a859d4e172a0150
QuCoon AI, DA & ML <machinelearningteam@qucoon.com>
2025-03-21 19:09:41 +0100

    Initial commit for RestaurantManager

---
+++ src/test/kotlin/com/pwb/restaurantManager/RestaurantManagerApplicationTests.kt
@@ -0,0 +1,15 @@
+//TODO ==== BUILT WITH QOONITY VERSION-1.0 - YINKA-ANTHONY-NKAY ON 2025-03-21T18:08:05.898737
+package com.pwb.restaurantManager
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RestaurantManagerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
+
