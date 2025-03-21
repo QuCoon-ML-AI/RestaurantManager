116662f17433d3356145ba816c803e457ced75b2
QuCoon AI, DA & ML <machinelearningteam@qucoon.com>
2025-03-21 19:09:34 +0100

    Initial commit for RestaurantManager

---
+++ src/main/kotlin/com/pwb/restaurantManager/utility/constant/PrefixConstant.kt
@@ -0,0 +1,6 @@
+//TODO ==== BUILT WITH QOONITY VERSION-1.0 - YINKA-ANTHONY-NKAY ON 2025-03-21T18:08:05.851191600
+package com.pwb.restaurantManager.utility.constant
+
+object PrefixConstant {
+    const val ENTITY = "DBO."
+}
